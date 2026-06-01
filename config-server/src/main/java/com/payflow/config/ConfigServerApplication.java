d74f0bf17f0ff162dd918e3cb267968ee6826d40
Raghuvanshi kushwah <kushwahraghuvanshi32@gmail.com>
2026-06-01 10:01:33 +0530

    cenralized  configuration

---
+++ config-server/src/main/java/com/payflow/config/ConfigServerApplication.java
@@ -0,0 +1,19 @@
+package com.payflow.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ConfigServerApplication.class, args);
+
+		System.out.println("Running app!!");
+	}
+
+}
