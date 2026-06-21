f8dc59ff03cf52adfd2d731bcfa01cf3a9ddfe8c
Raghuvanshi kushwah <kushwahraghuvanshi32@gmail.com>
2026-06-21 18:38:32 +0530

    gateway validate jwtToken and send payLoads into headers!

---
+++ gateway-service/src/main/java/com/payflow/gateway/security/JwtAuthFilter.java
@@ -0,0 +1,83 @@
+package com.payflow.gateway.security;
+
+import lombok.RequiredArgsConstructor;
+import org.apache.http.HttpHeaders;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthFilter implements GlobalFilter {
+
+    private final JwtUtil jwtUtil;
+
+    private static final List<String> PUBLIC_ENDPOINTS = List.of(
+            "/api/v1/auth/login",
+            "/api/v1/auth/register",
+            "/api/v1/auth/refresh"
+    );
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+
+        ServerHttpRequest request = exchange.getRequest();
+
+        String path = request.getURI().getPath();
+
+        // if the public endpoint are matched than we will bypass the filter
+        if (PUBLIC_ENDPOINTS.stream().anyMatch(path::startsWith)){
+            return chain.filter(exchange);
+        }
+
+        String authHeader = request.getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
+
+        if (authHeader == null || !authHeader.startsWith("Bearer")){
+            exchange.getResponse()
+                    .setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
+        }
+
+        try{
+            String token = jwtUtil.extractTokenFromHeader(authHeader);
+
+            String email = jwtUtil.extractEmail(token);
+
+            if (!jwtUtil.isTokenValid(token,email)){
+                exchange.getResponse()
+                        .setStatusCode(HttpStatus.UNAUTHORIZED);
+                return exchange.getResponse().setComplete();
+            }
+
+            Long userId = jwtUtil.extractUserId(token);
+
+            String role = jwtUtil.extractRole(token);
+
+            ServerHttpRequest mutatedRequest = request.mutate()
+                    .header("X-User-Id", String.valueOf(userId))
+                    .header("X-User-Email", email)
+                    .header("X-User-Role", role)
+                    .build();
+
+            // we are sending the header data to the down services
+            ServerWebExchange mutatedExchange = exchange.mutate()
+                    .request(mutatedRequest)
+                    .build();
+
+            return chain.filter(mutatedExchange);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            exchange.getResponse()
+                    .setStatusCode(HttpStatus.UNAUTHORIZED);
+
+            return exchange.getResponse().setComplete();
+        }
+    }
+}
